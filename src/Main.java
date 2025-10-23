f77cec6b72fc7f13117eeeef4c157df029098d92
LucasKeszler <lucas.keszler@gmail.com>
2025-10-23 13:36:32 +0200

    branch1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hej gutter");
-        lolololo
+import jdk.swing.interop.SwingInterOpUtils;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main (String[] args){
+
+
+        Scanner Bestilling = new Scanner(System.in);
+        initMenu();
+        loadOrdersFromFile();
+
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n1. Tilføj bestilling");
+            System.out.println("2. Fjern afhentet bestilling");
+            System.out.println("3. Afslut");
+            System.out.println();
+            System.out.println();
         }
+
+        String pizza = Bestilling.nextLine();
+
+
     }
+}
