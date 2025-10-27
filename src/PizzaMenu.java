a83eee980241fc8b438f709c59144e2352df79bc
jancervantesvasquez <janorlando3003@hotmail.com>
2025-10-27 12:07:49 +0100

    hello her er pizzamenu.

---
+++ src/PizzaMenu.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PizzaMenu {
+
+    private List<Pizza> menu;
+
+    public PizzaMenu(){
+        menu = new ArrayList<>(List.of(
+                new Pizza("Vesuvio:", 57),
+                new Pizza("Amerikaner:", 53),
+                new Pizza("Cacciatore:", 57),
+                new Pizza("Carbona:", 63),
+                new Pizza("Dennis:", 65),
+                new Pizza("Bertil:", 57),
+                new Pizza("Silvia:", 61),
+                new Pizza("Victoria:", 61),
+                new Pizza("Toronfo:", 61),
+                new Pizza("Capricciosa:", 61),
+                new Pizza("Hawaii:", 61),
+                new Pizza("Le Blissola:", 61),
+                new Pizza("Venezia", 61),
+                new Pizza("Mafia", 61)
+
+
+
+
+
+
+        ));
+    }
+}
